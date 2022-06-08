9fbb4e5a2cd55e696017d7718382fb38a157a1f5
Carolina Pirela <caropi30@gmail.com>
2022-06-08 12:52:13 -0400

    app

--- src/components/NewsletterSubscribtion/index.jsx
+++ src/components/NewsletterSubscribtion/index.jsx
@@ -4,36 +4,42 @@ import "./index.scss";
 const NewsletterSubscription = () => {
   return (
     <>
-      <section className="newsletter-subscription">
-        <div className="newsletter-subscription--txt">
-          <h3 className="newsletter-subscription--txt-title">
-            Subscribe to Newsletters
-          </h3>
-          <p className="newsletter-subscription--txt-paragraph">
-            Be aware of upcoming sales and events. Receive gifts and special
-            offers!
-          </p>
-        </div>
-        <form
-          action=""
-          method="post"
-          className="
+      <section className="newsletter">
+        <div className="newsletter-subscription">
+          <div className="newsletter-subscription--txt">
+            <h3 className="newsletter-subscription--txt-title">
+              Subscribe to Newsletters
+            </h3>
+            <p className="newsletter-subscription--txt-paragraph">
+              Be aware of upcoming sales and events. Receive gifts and special
+              offers!
+            </p>
+          </div>
+          <form
+            action=""
+            method="post"
+            className="
           newsletter-subscription--form"
-        >
-          <label
-            htmlFor="email"
-            className="
-          newsletter-subscription--form-email"
           >
-            <input type="email" name="email" placeholder="Ingresa tu correo" />
-          </label>
-          <input
-            type="submit"
-            value="SUBSCRIBE"
-            className="
+            <label
+              htmlFor="email"
+              className="
+          newsletter-subscription--form-email"
+            >
+              <input
+                type="email"
+                name="email"
+                placeholder="Ingresa tu correo"
+              />
+            </label>
+            <input
+              type="submit"
+              value="SUBSCRIBE"
+              className="
           newsletter-subscription--form-subscribe"
-          />
-        </form>
+            />
+          </form>
+        </div>
       </section>
     </>
   );
--- src/components/SpecialistGalleryContainer/index.jsx
+++ src/components/SpecialistGalleryContainer/index.jsx
@@ -5,23 +5,25 @@ import { specialists } from "../../data";
 const SpecialistGalleryContainer = () => {
   return (
     <>
-      <section className="specialist-gallery">
-        <div className="specialist-gallery--title">
-          <h2>
-            PROFESIONALES <span>ESPECIALISTAS</span>
-          </h2>
-        </div>
-        <div className="specialist-gallery--images">
-          {specialists.map((specialist) => {
-            return (
-              <img
-                className={"item-" + specialist.id}
-                src={specialist.image}
-                alt="specialist"
-                key={specialist.id}
-              />
-            );
-          })}
+      <section className="specialist">
+        <div className="specilist-gallery">
+          <div className="specialist-gallery--title">
+            <h2>
+              PROFESIONALES <span>ESPECIALISTAS</span>
+            </h2>
+          </div>
+          <div className="specialist-gallery--images">
+            {specialists.map((specialist) => {
+              return (
+                <img
+                  className={"item-" + specialist.id}
+                  src={specialist.image}
+                  alt="specialist"
+                  key={specialist.id}
+                />
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
